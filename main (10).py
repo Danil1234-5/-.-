69991ac2b016e998a95eb6f0374616e5b3cad251
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-17 11:29:04 +0300

    Классная работа 17.02.23

---
+++ main (10).py
@@ -0,0 +1,13 @@
+a = int(input())
+if a > 0:
+    print("Число больше нуля")
+else:
+    print("Число меньше нуля")
+    
+    
+    
+a = int(input())
+if a > 0:
+    print(1)
+else:
+    print(-1)   
