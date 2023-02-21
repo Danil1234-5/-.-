715a984ac14f9fe92bb69102f865caa3dc9319da
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-21 11:10:19 +0300

    Классная работа 21.02.23

---
+++ main (14).py
@@ -0,0 +1,9 @@
+a = int(input())
+num = a // 1000
+num1 = (a // 100) % 10
+num2 = (a // 10) % 10
+num3 = (a // 1) % 10
+b = num * num1 * num2 * num3
+summa =  num + num1 + num2 + num3
+print(b)
+print(summa)
