ec6a82f16ce438e07f04545014fb218af6b495b5
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-28 11:07:59 +0300

    Обновление работы от 21.02.23 Задание 1

---
+++ main (12).py
@@ -0,0 +1,12 @@
+a = int(input())
+if 999 < a < 10000
+num = a // 1000
+num1 = (a // 100) % 10
+num2 = (a // 10) % 10
+num3 = (a // 1) % 10
+b = num * num1 * num2 * num3
+summa =  num + num1 + num2 + num3
+print(b)
+print(summa)
+else: 
+    print(Введено не правильное число)
