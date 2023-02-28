e0f42d3fb4ae6644d4c63c7b74cac1699e81b01d
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-28 11:20:36 +0300

    Точно Обновление работы от 21.02.23 Задание 1

--- main (17).py
+++ main (17).py
@@ -1,21 +1,12 @@
 a = int(input())
-num = a // 100000
-num0 = (a // 10000) % 10
-num1 = (a // 1000) % 10
-num2 = (a // 100) % 10
-num3 = (a // 10) % 10
-num4 = (a // 1) % 10
-if num == 2:
-    print("2 входит")
-if num0 == 2:
-    print("2 входит")
-if num1 == 2:
-    print("2 входит")
-if num2 == 2:
-    print("2 входит")
-if num3 == 2:
-    print("2 входит")
-if num4 == 2:
-    print("2 входит")
-else:
-    print("2 не входит")
+if 999 < a < 10000:
+    num = a // 1000
+    num1 = (a // 100) % 10
+    num2 = (a // 10) % 10
+    num3 = (a // 1) % 10
+    b = num * num1 * num2 * num3
+    summa =  num + num1 + num2 + num3
+    print(b)
+    print(summa)
+else: 
+    print(Введено не правильное число)
