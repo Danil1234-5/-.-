5fa5c111af8526be0971177c0aaaa730359b0065
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-28 11:15:18 +0300

    Обновление работы от 21.02.23 Задание 3

--- main (15).py
+++ main (15).py
@@ -1,8 +1,11 @@
 a = int(input())
-num = a // 10000
-num0 = (a // 1000) % 10
-num1 = (a // 100) % 10
-num2 = (a // 10) % 10
-num3 = (a // 1) % 10
-print(num2)
-print(num3)
+if 99 < a < 1000:
+    num = a // 100
+    num0 = (a // 10) % 10
+    num1 = (a // 1) % 10
+    num1 = num1 *100
+    num0 = num0 * 10
+    c = num1 + num0 + num
+print(a - c)
+else:
+    print(Нет)
