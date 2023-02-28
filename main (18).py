025de249c55578e9a7c77889e40ec4ab4c0768dc
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-02-28 11:21:35 +0300

    Точно обновление работы от 21.02.23 Задание 2

---
+++ main (18).py
@@ -0,0 +1,11 @@
+a = int(input())
+if 9999 < a < 100000:
+    num = a // 10000
+    num0 = (a // 1000) % 10
+    num1 = (a // 100) % 10
+    num2 = (a // 10) % 10
+    num3 = (a // 1) % 10
+    print(num2)
+    print(num3)
+else:
+    print(Неправильное число)
