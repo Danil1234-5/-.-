f59ade027310785de42d03c820c56305ebf64670
Danil1234-5 <124032896+Danil1234-5@users.noreply.github.com>
2023-03-14 12:04:54 +0300

    Классная работа от 14.03.2023

--- main (17).py
+++ main (17).py
@@ -1,12 +1,11 @@
-a = int(input())
-if 999 < a < 10000:
-    num = a // 1000
-    num1 = (a // 100) % 10
-    num2 = (a // 10) % 10
-    num3 = (a // 1) % 10
-    b = num * num1 * num2 * num3
-    summa =  num + num1 + num2 + num3
-    print(b)
-    print(summa)
-else: 
-    print(Введено не правильное число)
+A, B, C, =float(input()), float(input()), float(input())
+pi = 3.14
+x = 0
+if B // C <= pi // 4:
+    print(x)
+else:
+    print(x * 1)
+if x <= pi // 4:
+    print("y =", "sinx")
+else:
+    print("y =", "cosx")
